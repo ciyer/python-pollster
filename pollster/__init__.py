022a1e0d90a3c56cfc1eb59da7bea683c03896fc
Adam Hooper <adam@adamhooper.com>
2016-08-22 14:01:38 -0400

    Bump again, to 0.1.5

--- pollster/__init__.py
+++ pollster/__init__.py
@@ -3,6 +3,6 @@ Methods for accessing the HuffPost Pollster API. Documentation for this API
 may be found at http://elections.huffingtonpost.com/pollster/api.
 """
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .pollster import Pollster, Chart, Poll, PollsterException
